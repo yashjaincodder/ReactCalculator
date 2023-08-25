bee2296a7f283d7b3ca94397c0f10b2cd6ae4e28
yash jain <yjain2529@gmail.com>
2023-08-25 14:12:47 +0530

    mern calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[value,setvalue]=useState("");
+  const buttonhandler=(item)=>{
+  if(item==="C"){
+    setvalue(" ");
+  }
+  else if(item==="<"){
+    setvalue(value.slice(0,-1));
+  }
+  else if(item==="="){
+    setvalue(eval(value).toString());
+  }
+  else{
+   setvalue((preitem)=>preitem+item)
+  }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calc'>
+        <input type="text" name='input' value={value} />
+        <div className='butt'>
+          <button onClick={()=>buttonhandler("C")}>C</button>
+          <button onClick={()=>buttonhandler("<")}>&lt;</button>
+          <button onClick={()=>buttonhandler("%")}>%</button>
+          <button onClick={()=>buttonhandler("/")}>/</button>
+          <button onClick={()=>buttonhandler("9")}>9</button>
+          <button onClick={()=>buttonhandler("8")}>8</button>
+          <button onClick={()=>buttonhandler("7")}>7</button>
+          <button onClick={()=>buttonhandler("+")}>+</button>
+          <button onClick={()=>buttonhandler("6")}>6</button>
+          <button onClick={()=>buttonhandler("5")}>5</button>
+          <button onClick={()=>buttonhandler("4")}>4</button>
+          <button onClick={()=>buttonhandler("-")}>-</button>
+          <button onClick={()=>buttonhandler("3")}>3</button>
+          <button onClick={()=>buttonhandler("2")}>2</button>
+          <button onClick={()=>buttonhandler("1")}>1</button>
+          <button onClick={()=>buttonhandler("*")}>*</button>
+          <button onClick={()=>buttonhandler("0")}>0</button>
+          <button onClick={()=>buttonhandler("00")}>00</button>
+          <button onClick={()=>buttonhandler(".")}>.</button>
+          <button onClick={()=>buttonhandler("=")}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
